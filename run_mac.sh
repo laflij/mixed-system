dc5e3895979e343ba533b4ca32d82086823e8b9b
laflij <laflijj@gmail.com>
2013-10-27 16:31:14 -0400

    multiphase run working

--- run_mac.sh
+++ run_mac.sh
@@ -6,10 +6,9 @@
 
 # The script uses a serial version of lammps.
 
-if [ $# != 2 ]
+if [ $# != 1 ]
 then
     echo 'Argument Error: Need one argument'
-    echo 'Usage: sh run.sh folder <simpart>'
     exit
 fi
 
@@ -27,23 +26,7 @@ fi
 
 cd $folder
 
-if [ $simpart = 0 ]
-then
-    logfile=md-0.log
-    runfile=${folder}.run
+logfile=md-0.log
+runfile=${folder}.run
 #     $lmppath/$lmpexec  -log $logfile < $runfile
-    mpirun -np 4 $lmppath/$lmpexec -log $logfile < $runfile
-else
-    oldpart=`echo $simpart | awk '{print $1-1}'`
-    if [ ! -f confout-$oldpart.data ]
-    then
-	echo "Error: Missing restart file from simpart '$oldpart'"
-	exit
-    fi 
-    logfile=md-$simpart.log
-    sed "s/SIMPART/$simpart/g;s/OLDPART/$oldpart/g" ${folder}-continue.run \
-	> ${folder}-$simpart.run
-    runfile=${folder}-$simpart.run
-#     $lmppath/$lmpexec -log $logfile < $runfile
-    mpirun -np 4 $lmppath/$lmpexec -log $logfile < $runfile
-fi
+mpirun -np 4 $lmppath/$lmpexec -log $logfile < $runfile
