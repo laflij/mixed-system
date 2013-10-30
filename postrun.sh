4858f41bbc44750ea8c4f079a8f9d240412ac5ac
laflij <laflijj@gmail.com>
2013-10-29 21:07:56 -0400

    force applied to folded system working

--- postrun.sh
+++ postrun.sh
@@ -1,32 +1,29 @@
 # Get folder of run
-if [ $# != 1 ]
+if [ $# != 2 ]
 then
-    echo 'Argument Error: Need Folder'
+    echo 'Argument Error: Need one argument'
+    echo 'Usage: ./postrun folder <simpart>'
     exit
 fi
 
-folder=$1
+fifolder=$1
+simpart=$2
 
 # Remove state data files
 rm -rf $folder/state_cpt-*
 
-# Get current date and time
-time_stamp=$(date +%d-%m-%Y_%H-%M)
-
 # Make a folder for run data files
-runfolder="run-$time_stamp"
+runfolder="run-$simpart"
 mkdir -p $folder/$runfolder
 
 # Move all data from run to this folder
+mv $folder/$folder-$simpart.run $folder/$runfolder/
 mv $folder/*.lammpstrj $folder/$runfolder/
 mv $folder/*.xtc $folder/$runfolder/
 mv $folder/*.xvg $folder/$runfolder/
 mv $folder/*log* $folder/$runfolder/
 
+# Copy binary data file 
+cp $folder/confout-$simpart.data $folder/$runfolder/
 # Copy plot files for ease
 cp $folder/*.gnu $folder/$runfolder/
-
-# Copy run file and data file for repeatability
-cp $folder/$folder.run $folder/$runfolder/
-cp $folder/$folder.data $folder/$runfolder/
- 
